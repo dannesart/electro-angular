5f35ca4a590df4421964ed600addeb478b254c84
dannesart <dannesart@live.com>
2023-05-26 14:36:09 +0200

    Adding firebase and basic menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  collection,
+  doc,
+  collectionData,
+  Firestore,
+  docData,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'electro';
+  users$: Observable<any[]>;
+  menu$: Observable<any>;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    const menuDoc = doc(this.firestore, '/data/new-model-menu/items/main');
+    const usersCollection = collection(this.firestore, 'users');
+    this.menu$ = docData(menuDoc);
+    this.users$ = collectionData(usersCollection);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyDP4u0xemmWPG-cAfzy5SXI2gk-9a0VMrg',
+        authDomain: 'eaglecms-55a84.firebaseapp.com',
+        databaseURL: 'https://eaglecms-55a84.firebaseio.com',
+        projectId: 'eaglecms-55a84',
+        storageBucket: 'eaglecms-55a84.appspot.com',
+        messagingSenderId: '413530424879',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
